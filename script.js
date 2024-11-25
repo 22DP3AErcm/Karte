f85a8ec74a98bbb4934a15f8eae9f9b63181f7f4
22DP3AErcm <131261272+22DP3AErcm@users.noreply.github.com>
2024-11-25 21:44:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Inicializē karti
+    var map = L.map('map').setView([56.8796, 24.6032], 8); // Centrs uz Latviju
+
+    // Pievieno OpenStreetMap kartes slāni
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Ielādē datus no geomap.json
+    fetch('geomap.json')
+        .then(response => response.json())
+        .then(data => {
+            data.features.forEach(feature => {
+                if (feature.properties.PLACESUBTY === 2) { // Filtrē tikai atpūtas vietas
+                    var coordinates = feature.geometry.coordinates;
+                    var latLng = LKS92WGS84.convertXYToLatLon(coordinates);
+                    var marker = L.marker([latLng[0], latLng[1]]).addTo(map);
+                    marker.bindPopup(`<b>${feature.properties.PLACENAME}</b><br>${feature.properties.PLACESUBTY}`);
+                }
+            });
+        })
+});
